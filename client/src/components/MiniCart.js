10dea923f959762e3707aa52b01acc173f58514d
Andresa Martins <contact@andresa.dev>
2021-08-17 07:34:46 -0300

    #10 — Create Shopping Cart with local storage

---
+++ client/src/components/MiniCart.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useCart } from "react-use-cart";
+import { Cart } from 'react-bootstrap-icons';
+
+/**
+ * Mini Shopping Cart
+ * @param products
+ * @returns {JSX.Element}
+ * @constructor
+ */
+export const MiniCart = ({products}) => {
+    const {
+        cartTotal,
+        totalUniqueItems
+    } = useCart();
+
+    const formattedTotal = (Math.round(cartTotal * 100) / 100).toFixed(2);
+
+    return (
+        <>
+            <p>
+                <Cart/> {totalUniqueItems} - ${formattedTotal}
+            </p>
+        </>
+    );
+}
